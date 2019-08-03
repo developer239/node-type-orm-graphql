7ecde9644a5743a701629bc201504078451c9fcb
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-03 15:59:46 +0200

    chore: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'typescript',
+    'import',
+  ],
+  extends: [
+    '@strv/node/v10',
+    '@strv/node/style',
+    '@strv/node/optional',
+    '@strv/typescript',
+    '@strv/typescript/style',
+    'prettier',
+  ],
+  settings: {
+    'import/resolver': {
+      'typescript': {},
+    },
+  },
+  rules: {
+    'import/group-exports': 0,
+    'node/no-unsupported-features/es-syntax': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    'new-cap': 0,
+  },
+}
