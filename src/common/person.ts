8116189089a918b93418d6439703c45eee3286da
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-03 15:59:45 +0200

    feat: add minimal express server

--- src/common/person.ts
+++
@@ -1,7 +0,0 @@
-interface Person {
-  name: string
-}
-
-export const person: Person = {
-  name: 'Michal',
-}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import { person } from '~/common/person'
+import express from 'express'
+import * as bodyParser from 'body-parser'
 
-console.log(`Hello ${person.name}`)
+// create and setup express app
+const app = express()
+app.use(bodyParser.json())
+
+// start express server
+app.listen(3000, () => {
+  console.log('Started server on port 3000')
+})
