c90fd16376cbf2c3d13e43ed6654af10ff6672c8
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-03 15:59:48 +0200

    feat: create higher order create entity resolver

---
+++ src/modules/Core/resolvers/createResolver.ts
@@ -0,0 +1,29 @@
+import { Resolver, Mutation, Arg, UseMiddleware, ClassType, Ctx } from 'type-graphql'
+import { Middleware } from 'type-graphql/dist/interfaces/Middleware'
+import { IAppContext } from '~/types'
+
+export const createResolver = <IInputType extends ClassType>(
+  entity: any,
+  inputType: IInputType,
+  middleware?: Middleware<any>[]
+) => {
+  @Resolver()
+  class BaseResolver {
+    @Mutation(() => entity, { name: `create${entity.className}` })
+    @UseMiddleware(...(middleware || []))
+    create(@Arg('data', () => inputType) data: any, @Ctx() ctx: IAppContext) {
+      if (entity.belongsToUser) {
+        return entity
+          .create({
+            ...data,
+            user: { id: ctx.req.session.userId },
+          })
+          .save()
+      }
+
+      return entity.create(data).save()
+    }
+  }
+
+  return BaseResolver
+}
