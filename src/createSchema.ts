5a2f0f29b76291c64738c010c7e83c22166ed69f
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-03 15:59:46 +0200

    feat: add type-graphql

---
+++ src/createSchema.ts
@@ -0,0 +1,16 @@
+import { buildSchema, Query, Resolver } from 'type-graphql'
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async helloWorld() {
+    return 'Hello World!'
+  }
+}
+
+export const createSchema = () =>
+  buildSchema({
+    resolvers: [
+      HelloResolver,
+    ],
+  })
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import 'reflect-metadata'
 import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
 import * as bodyParser from 'body-parser'
 import { createConnection } from 'typeorm'
+import { createSchema } from '~/createSchema'
 
 const app = express()
+
 app.use(bodyParser.json())
 
 const main = async () => {
   await createConnection()
+
+  const apolloServer = new ApolloServer({
+    schema: await createSchema(),
+    context: ({ req, res }: any) => ({ req, res }),
+  })
+
+  apolloServer.applyMiddleware({ app })
 
   app.listen(3000, () => {
     console.log('Started server on port 3000')
