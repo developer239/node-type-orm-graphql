a639aca24e031987d066e0ec754eab9f50c20318
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-03 15:59:45 +0200

    feat: add type-orm

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
+import 'reflect-metadata'
 import express from 'express'
 import * as bodyParser from 'body-parser'
+import { createConnection } from 'typeorm'
 
-// create and setup express app
 const app = express()
 app.use(bodyParser.json())
 
-// start express server
-app.listen(3000, () => {
-  console.log('Started server on port 3000')
-})
+const main = async () => {
+  await createConnection()
+
+  app.listen(3000, () => {
+    console.log('Started server on port 3000')
+  })
+}
+
+main()
