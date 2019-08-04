d3dbd9b1d6477bb6b7d9b1fd9f9ec56f923353d2
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-05 00:51:34 +0200

    feat: mask all graphql errors

--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,15 @@ const main = async () => {
     introspection: true,
     playground: true,
     plugins: [createComplexityValidator(schema)],
+    formatError: error => {
+      return new Error(error.message)
+    },
   })
 
   apolloServer.applyMiddleware({ app, cors: false })
 
   app.listen(config.server.port, () => {
-    console.log(`Started server on port ${config.server.port}`)
+    console.log(`[info] Started server on port ${config.server.port}`)
   })
 }
 
